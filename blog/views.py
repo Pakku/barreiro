459fc3658d3510d5db777e2b46d687bbf038b941
Carlos Barreiro García <quentyaiden@gmail.com>
2020-06-09 15:36:45 -0500

    Basic blog with list and detail of posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_at')
+    template_name = 'blog/index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
