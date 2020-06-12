8dd45faa30af3292d21ed3c1f548e127ee08b7ef
Carlos Barreiro García <quentyaiden@gmail.com>
2020-06-11 19:54:01 -0500

    Pagination

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from blog.models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_at')
     template_name = 'blog/index.html'
+    paginate_by = 1
 
 
 def post_detail(request, slug):
