419f1a3d0bf8d32722886d064a2957e0c5ee3d88
Carlos Barreiro García <quentyaiden@gmail.com>
2020-06-15 15:23:33 -0500

    Styles for post list.

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,7 @@ let mix = require('laravel-mix');
 mix.setPublicPath('blog')
    .js('blog/resources/js/app.js', 'blog/static/blog/js')
    .sass('blog/resources/scss/app.scss', 'blog/static/blog/css')
+    .options({
+        processCssUrls: false
+    })
 ;
